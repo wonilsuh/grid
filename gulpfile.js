795b13241c0ab09279c4f4eabc564b02ecccafb6
James Y. Rauhut <jamesrauhut@us.ibm.com>
2017-10-04 09:49:51 -0500

    Add IBM Type to tutorial

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,13 @@
 const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
 const cleanCSS = require('gulp-clean-css');
+const del = require('del');
 const rename = require('gulp-rename');
 const sass = require('gulp-sass');
+
+gulp.task('clean', function () {
+	return del(['dist/**/*']);
+});
 
 gulp.task('styles', function() {
      gulp.src("src/ibm-grid.scss")
@@ -15,7 +20,12 @@ gulp.task('styles', function() {
       }))
       .pipe(rename('ibm-grid.min.css'))
       .pipe(gulp.dest('dist'))
-      .pipe(gulp.dest('docs'));
+      .pipe(gulp.dest('docs/css'));
+});
+
+gulp.task('type', function () {
+	gulp.src("node_modules/@ibm/type/dist/**/*")
+		.pipe(gulp.dest('docs/css/@ibm/type'));
 });
 
 gulp.task('watch',function() {
@@ -29,3 +39,5 @@ gulp.task('watch',function() {
     gulp.watch("dist/**/*").on("change", browserSync.reload);
     gulp.watch("docs/**/*").on("change", browserSync.reload);
 });
+
+gulp.task('default', ['clean', 'type', 'styles', 'grid']);
